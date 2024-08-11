5c4fa11fbb753936c370edee2fe1967cd890255b
“Anish508” <anishbarke9741@gmail.com>
2024-08-11 10:30:26 +0530

    components reusability and forward ref

--- Complete-React-App/megaBlog-appwrite/src/App.jsx
+++ Complete-React-App/megaBlog-appwrite/src/App.jsx
@@ -1,16 +1,42 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import {useDispatch} from "react-redux"
+import authService from "./Appwrite/auth";
+import { login , logout } from "./store/authSlice";
+import { Header, Footer } from "./components";
+import {Outlet} from "react-router-dom"
+
 
 function App() {
-console.log(import.meta.env.VITE_APPWRITE_URL)
+  const [loading , setLoading] = useState(true)
+  const dispatch = useDispatch
+  useEffect(()=>{
+    authService.getCurrentUser()
+    .then(((userData)=>{
+      if (userData) {
+        dispatch(login({userData}))
+      }else{
+        dispatch(logout())
+      }
+    }))
+    .catch((error)=>console.log("Error: unable to fetch user data", error)
+    )
+    .finally(()=>setLoading(false))
+  } , [])
 
-  return (
-    <>
-     <h1>A Blog app with appwrite</h1>
-    </>
-  )
+  return !loading ? (
+    <div className="min-h-screen flex flex-wrap content-between bg-gray-600">
+      <div className="w-full block">
+        <Header></Header>
+        <main>
+          Todo:{/* <Outlet/> */}
+        </main>
+        <Footer></Footer>
+      </div>
+    </div>
+  ) : null
 }
 
-export default App
+export default App;
--- Complete-React-App/megaBlog-appwrite/src/Appwrite/auth.js
+++ Complete-React-App/megaBlog-appwrite/src/Appwrite/auth.js
@@ -1,5 +1,6 @@
-import conf from "../conf/conf";
+import conf from "../conf/conf.js";
 import { Client, Account, ID } from "appwrite";
+
 
 export class AuthService {
   client = new Client();
@@ -58,6 +59,6 @@ export class AuthService {
   }
 }
 
-const AuthService = new AuthService();
+const authService = new AuthService();
 
-export default AuthService;
+export default authService;
